b36d37c27884617c6b304ae42c51e844e54ef60f
Marat Ayaibergenov <marataya@gmail.com>
2024-06-10 09:25:53 +0500

    UnscrambleApp: 1st commit

---
+++ UnscrabbleApp/app/src/main/java/com/example/unscrabbleapp/ui/GameScreen.kt
@@ -0,0 +1,10 @@
+package com.example.unscrabbleapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+
+@Composable
+fun GameScreen(gameViewModel: Game) {
+
+}
---
+++ UnscrabbleApp/app/src/main/java/com/example/unscrabbleapp/ui/GameViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.unscrabbleapp.ui
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+
+class GameViewModel: ViewModel() {
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    var userGuess by mutableStateOf("")
+        private set
+
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+}

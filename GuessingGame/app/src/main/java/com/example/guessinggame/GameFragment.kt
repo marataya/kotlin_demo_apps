3c1d40d0a64c56dc3c065000d60327c1d51ea3d9
Marat Ayaibergenov <marataya@gmail.com>
2024-07-02 19:08:00 +0500

    GuessingGame: add Game and Result fragments, navigation and game logic into GameFragment

---
+++ GuessingGame/app/src/main/java/com/example/guessinggame/GameFragment.kt
@@ -0,0 +1,143 @@
+package com.example.guessinggame
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.guessinggame.databinding.FragmentGameBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GameFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GameFragment : Fragment() {
+    private var _binding: FragmentGameBinding? = null
+    private val binding get() = _binding!!
+
+    val words = listOf("Android", "Activity", "Fragment")
+    val secretWord = words.random().uppercase()
+    var secretWordDisplay = ""
+    var correctGuesses = ""
+    var incorrectGuesses = ""
+    var triesLeft = 0
+
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        secretWordDisplay = deriveSecretWordDisplay()
+        updateScreen()
+
+        binding.guessButton.setOnClickListener {
+            makeGuess(binding.guess.text.toString().uppercase())
+            binding.guess.text = null
+            updateScreen()
+            if (isWon() || isLost()) {
+                val action = GameFragmentDirections.actionGameFragmentToResultFragment(wonLostMessage())
+                view.findNavController().navigate(action)
+            }
+        }
+
+        return view
+    }
+
+    private fun wonLostMessage(): String {
+        var message = ""
+        if (isWon()) message = "You won!"
+        else if (isLost()) message = "You lost!"
+        message += " The word was $secretWord."
+        return message
+    }
+
+    private fun isWon(): Boolean = secretWord.equals(secretWordDisplay, true)
+
+    private fun isLost(): Boolean = triesLeft <= 0
+
+    /**
+     * called every time the user makes a guess
+     */
+    private fun makeGuess(guess: String) {
+        if (guess.length == 1) {
+            if (secretWord.contains(guess)) {
+                correctGuesses += guess
+                if (correctGuesses.length == secretWord.length) {
+                    correctGuesses += guess
+                    secretWordDisplay = deriveSecretWordDisplay()
+                }
+            } else {
+                incorrectGuesses += "$guess "
+                triesLeft--
+            }
+        }
+    }
+
+    private fun updateScreen() {
+        binding.word.text = secretWordDisplay
+        binding.tries.text = "You have $triesLeft guesses left"
+        binding.incorrectGuesses.text = "Incorrect guesses: $incorrectGuesses"
+    }
+
+    /**
+     * Build a string for how the secret word should be displayed on the screen
+     */
+    private fun deriveSecretWordDisplay(): String {
+        var display = ""
+        secretWord.forEach {
+            display += checkLetter(it.toString())
+        }
+        return display
+    }
+
+    private fun checkLetter(str: String) = when(correctGuesses.contains(str)) {
+        true -> str
+        false -> "_"
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment GameFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            GameFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}

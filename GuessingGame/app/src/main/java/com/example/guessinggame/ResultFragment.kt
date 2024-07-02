e159a06e609da39ee505a559070dadc20739e88c
Marat Ayaibergenov <marataya@gmail.com>
2024-07-03 00:13:02 +0500

    GuessingGame: add ResultFragment layout and logic

--- GuessingGame/app/src/main/java/com/example/guessinggame/ResultFragment.kt
+++ GuessingGame/app/src/main/java/com/example/guessinggame/ResultFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.guessinggame.databinding.FragmentResultBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,7 +19,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ResultFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    private var _binding: FragmentResultBinding? = null
+    private val binding get() = _binding!!
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -33,8 +37,20 @@ class ResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        _binding = FragmentResultBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.wonLost.text = ResultFragmentArgs.fromBundle(requireArguments()).result
+
+        binding.newGameButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_resultFragment_to_gameFragment)
+        }
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
